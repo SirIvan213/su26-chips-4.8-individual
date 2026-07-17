0ae73c29bf79750e94d6525484a23cae833a337a
Ivan Pedroza-Machuca <ivanpedroza@berkeley.edu>
2026-07-17 22:11:20 +0000

    Added Render Build Script

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-
+Movie.destroy_all
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
   {:title => 'Aladdin', :rating => 'G',
